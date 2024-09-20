08f521657d9cf19938c6e5a1349cb97e99783102
jackmp18 <55199733+jackmp18@users.noreply.github.com>
2024-09-20 15:25:27 -0700

    Update StoryGrader.cs

--- OpenAiToy/StoryGrader.cs
+++ OpenAiToy/StoryGrader.cs
@@ -38,7 +38,7 @@ namespace JiraGPTGrader
                     // Define the necessary parameters for updating Jira
                     string jiraBaseUrl = "https://freightpop.atlassian.net";
                     string username = "jiraapiuser@freightpop.com";
-                    string apiToken = "ATATT3xFfGF0ju8nEW57JWBre8tn6nK5mmLyTWrqMBZNTfH2rX9EBm29R0J9uRO4v5n-jYbQ9hiYZR6BxOPjJFOYC6qfNfW5IjlHv1v9GY81tCrBpOFqFn3MusQ1-PMKG6_5RkDqeMj4Ku-T8gCAeYAZlTSRlltOnoWnCw8n1IKE-BVUiDRJ1Yc=0ED9CB68";
+                    string apiToken = Environment.GetEnvironmentVariable("Jira");;
 
                     // Update the Jira ticket with GPT rating and feedback
                     _jiraClient.UpdateJiraStoryWithFeedback(jiraBaseUrl, issueKey, username, apiToken, feedback.Score, feedback.Feedback);
